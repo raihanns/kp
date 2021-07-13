7dc98de9bb9fe3fd550be6518ef14c5f6332c9a6
raihanns <rainsdotnet@gmail.com>
2021-07-13 13:57:28 +0700

    ADMIN DASHBOARD FIX

---
+++ application/models/M_Auth.php
@@ -0,0 +1,54 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+class M_Auth extends CI_model
+{
+    public function login($email)
+    {
+        $user = $this->db->get_where('user', ['email' => $email])->row_array();
+        if ($user) {
+            // usernya ada
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function insertUser($data)
+    {
+        return $this->db->insert('user', $data);
+    }
+    public function getAllUser()
+    {
+        return $this->db->get('user')->result_array();
+    }
+
+    public function editUser($id)
+    {
+        $data = [
+            'nama_depan' => $this->input->post('namadepan', true),
+            'nama_belakang' => $this->input->post('namabelakang', true),
+            'email' => $this->input->post('email', true),
+            'password' => $this->input->post('password')
+        ];
+        $this->db->where('id', $id);
+        $this->db->update('user', $data);
+    }
+
+    public function deleteUser($id)
+    {
+        $this->db->where('id', $id);
+        $this->db->delete('user');
+    }
+
+    public function getUserById($id)
+    {
+        $this->db->where('id', $id);
+        return $this->db->get('user')->row_array();
+    }
+
+
+    public function getSession()
+    {
+        return $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+    }
+}
---
+++ application/views/admin/index.php
@@ -0,0 +1,28 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+
+    <div class="row">
+        <div class="col-lg-5">
+            <div class="card shadow mb-4">
+                <!-- Card Header - Dropdown -->
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                    <h6 class="m-0 font-weight-bold text-primary">Test Card</h6>
+                </div>
+                <!-- Card Body -->
+                <div class="card-body">
+                    <!-- isi -->
+                    <div class="chart-area"></div>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
