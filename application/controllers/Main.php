c3a23bd8d7f92050d38bbf958e83d4ed5842adb1
Algi Erwangga <algi.erwangga99@gmail.com>
2021-07-16 14:18:51 +0700

    main

---
+++ application/controllers/Main.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+
+class Main extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        $data['title'] = 'Main';
+        // $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        // $this->load->view('templates/header', $data);
+        // $this->load->view('templates/sidebar', $data);
+        // $this->load->view('templates/topbar', $data);
+        $this->load->view('main', $data);
+        // $this->load->view('templates/footer', $data);
+    }
+}
