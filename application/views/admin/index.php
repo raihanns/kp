bde01d15b1f4be735a29db9b7098d9a40778ea42
raihanns <rainsdotnet@gmail.com>
2021-08-03 12:16:15 +0700

    Update index.php

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -5,7 +5,7 @@
     <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
 
 
-    <div class="card mb-4">
+    <div class="card shadow mb-4">
         <div class="card-header">
             Welcome, <?= $user['name']; ?>!
         </div>
