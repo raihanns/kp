e6731ae3b753317476503dc545aee5f03a319b41
Algi Erwangga <algi.erwangga99@gmail.com>
2021-08-10 14:57:37 +0700

    update data jalan

--- application/views/data_jalan.php
+++ application/views/data_jalan.php
@@ -77,8 +77,10 @@
         <div class="d-flex align-items-center p-3 my-3 text-black-50 bg-light rounded shadow-sm">
             <img src="<?= base_url('assets/img/'); ?>pemkab.png" alt="" width="48" height="48">
             <div class="lh-100">
-                <h4 class="mb-0 text-center-Black lh-100 ">DINAS PEKERJAAN UMUM DAN TATA RUANG</h4>
-                <medium>SOREANG</medium>
+                <h6 class="mb-0 text-center-Black lh-100 ">PEMERINTAH KABUPATEM BANDUNG</h6>
+                <h6 class="mb-0 text-center-Black lh-100 ">DINAS PEKERJAAN UMUM DAN TATA RUANG</h6>
+                <small>Jl. Raya Soreang - Banjaran KM. 3 Kabupaten Bandung</small>
+                <small>Telp. (002) 5892580</small>
             </div>
         </div>
 
@@ -99,6 +101,15 @@
                     <strong class="d-block text-gray-dark">No Ruas Jalan</strong>
                     00001152711
                 </p>
+                <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false">
+                    <title>Placeholder</title>
+                    <<rect width="100%" height="100%" fill="#2a9d8f" /><text x="50%" y="50%" fill="#ffffff" dy=".3em">5</text>
+                </svg>
+
+                <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+                    <strong class="d-block text-gray-dark">Panjang</strong>
+                    2 KM
+                </p>
             </div>
             <div class="media text-muted pt-3">
                 <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false">
@@ -109,6 +120,15 @@
                 <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
                     <strong class="d-block text-gray-dark">Nama Ruas Jalan</strong>
                     Bojong Soang
+                </p>
+                <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false">
+                    <title>Placeholder</title>
+                    <rect width="100%" height="100%" fill="#2a9d8f" /><text x="50%" y="50%" fill="#ffffff" dy=".3em">6</text>
+                </svg>
+
+                <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+                    <strong class="d-block text-gray-dark">Lebar</strong>
+                    2.5 M
                 </p>
             </div>
             <div class="media text-muted pt-3">
@@ -133,36 +153,12 @@
                     Dayeuh Kolot
                 </p>
             </div>
-            <div class="media text-muted pt-3">
-                <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false">
-                    <title>Placeholder</title>
-                    <<rect width="100%" height="100%" fill="#2a9d8f" /><text x="50%" y="50%" fill="#ffffff" dy=".3em">5</text>
-                </svg>
-
-                <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-                    <strong class="d-block text-gray-dark">Panjang</strong>
-                    2 KM
-                </p>
-                <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false">
-                    <title>Placeholder</title>
-                    <rect width="100%" height="100%" fill="#2a9d8f" /><text x="50%" y="50%" fill="#ffffff" dy=".3em">6</text>
-                </svg>
-
-                <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-                    <strong class="d-block text-gray-dark">Lebar</strong>
-                    2.5 M
-                </p>
-            </div>
         </div>
         </div>
 
         <div class="my-3 p-3 bg-light rounded shadow-sm">
             <h4 class="border-bottom border-gray pb-2 mb-0">Jenis Perkerasan</h4>
             <div class="media text-muted pt-3">
-                <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false">
-                    <title>Placeholder</title>
-                    <rect width="100%" height="100%" fill="#2a9d8f" /><text x="50%" y="50%" fill="#ffffff" dy=".3em">1</text>
-                </svg>
                 <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
                     <div class="d-flex justify-content-between align-items-center w-100">
                         <strong class="text-gray-dark">Jenis Aspal</strong>
@@ -218,10 +214,6 @@
         <div class="my-3 p-3 bg-light rounded shadow-sm">
             <h4 class="border-bottom border-gray pb-2 mb-0">Kondisi Jalan</h4>
             <div class="media text-muted pt-3">
-                <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false">
-                    <title>Placeholder</title>
-                    <rect width="100%" height="100%" fill="#2a9d8f" /><text x="50%" y="50%" fill="#ffffff" dy=".3em">1</text>
-                </svg>
                 <div class="media-body pb-3 mb-0  small lh-125 border-bottom border-gray">
                     <div class="d-flex justify-content-between align-items-center w-100">
                         <strong class="text-gray-dark">Kondisi Baik</strong>
