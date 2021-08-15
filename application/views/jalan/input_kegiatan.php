ed4e3f41c5c4e52ce19cd4e1c915e55f9bd53b3c
raihanns <rainsdotnet@gmail.com>
2021-08-15 14:23:11 +0700

    form

--- application/views/jalan/input_kegiatan.php
+++ application/views/jalan/input_kegiatan.php
@@ -10,27 +10,31 @@
 
                 <div class="mb-2">
                     <label for="ruasjalan" class="form-label">Nama Paket Kegiatan</label>
-                    <input type="text" class="form-control" id="ruasjalan" />
+                    <input type="text" class="form-control" id="kegiatan" name="kegiatan" />
                 </div>
                 <div class="mb-2">
                     <label for="ruasjalan" class="form-label">Perusahaan</label>
-                    <input type="text" class="form-control" id="ruasjalan" />
+                    <input type="text" class="form-control" id="perusahaan" name="peruysahaan" />
                 </div>
                 <div class="mb-2">
                     <label for="ruasjalan" class="form-label">Nilai Kontrak</label>
-                    <input type="text" class="form-control" id="ruasjalan" />
+                    <input type="text" class="form-control" id="kontrak" name="kontrak" />
+                </div>
+                <div class="mb-2">
+                    <label for="ruasjalan" class="form-label">Jangka Waktu Pelaksanaan</label>
+                    <input type="text" class="form-control" id="jangka" name="ruasjalan" />
                 </div>
                 <div class="mb-2">
                     <label for="ruasjalan" class="form-label">Volume Pekerjaan</label>
-                    <input type="text" class="form-control" id="ruasjalan" />
+                    <input type="text" class="form-control" id="volume_pekerjaan" name="ruasjalan" />
                 </div>
                 <div class="form-group">
                     <label for="exampleFormControlFile1">Dokumentasi Kegiatan</label>
-                    <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                    <input type="file" class="form-control-file" id="foto_kegiatan" name="ruasjalan">
                 </div>
                 <div class="form-group">
                     <label for="exampleFormControlFile1">Upload Video</label>
-                    <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                    <input type="file" class="form-control-file" id="video_kegiatan" name="ruasjalan">
                 </div>
                 <input class="btn btn-primary mt-3" type="submit" value="Submit">
             </div>
