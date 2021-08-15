89f9f676ef40e5fff7e001516219dbd5890b0556
YusrilAG <61075909+YusrilAG@users.noreply.github.com>
2021-08-15 15:08:20 +0700

    update database

--- application/views/jalan/input_kegiatan.php
+++ application/views/jalan/input_kegiatan.php
@@ -10,31 +10,31 @@
 
                 <div class="mb-2">
                     <label for="ruasjalan" class="form-label">Nama Paket Kegiatan</label>
-                    <input type="text" class="form-control" id="kegiatan" name="kegiatan" />
+                    <input type="text" class="form-control" id="kegiatan" name="kegiatan" value="<?= $jalan['kegiatan']; ?>" />
                 </div>
                 <div class="mb-2">
                     <label for="ruasjalan" class="form-label">Perusahaan</label>
-                    <input type="text" class="form-control" id="perusahaan" name="perusahaan" />
+                    <input type="text" class="form-control" id="perusahaan" name="perusahaan" value="<?= $jalan['perusahaan']; ?>" />
                 </div>
                 <div class="mb-2">
                     <label for="ruasjalan" class="form-label">Nilai Kontrak</label>
-                    <input type="text" class="form-control" id="kontrak" name="kontrak" />
+                    <input type="text" class="form-control" id="kontrak" name="kontrak" value="<?= $jalan['kontrak']; ?>" />
                 </div>
                 <div class="mb-2">
                     <label for="ruasjalan" class="form-label">Jangka Waktu Pelaksanaan</label>
-                    <input type="text" class="form-control" id="jangka" name="jangka" />
+                    <input type="text" class="form-control" id="jangka" name="jangka" value="<?= $jalan['jangka']; ?>" />
                 </div>
                 <div class="mb-2">
                     <label for="ruasjalan" class="form-label">Volume Pekerjaan</label>
-                    <input type="text" class="form-control" id="volume_pekerjaan" name="volume_pekerjaan" />
+                    <input type="text" class="form-control" id="volume_pekerjaan" name="volume_pekerjaan" value="<?= $jalan['volume_pekerjaan']; ?>" />
                 </div>
                 <div class="form-group">
                     <label for="exampleFormControlFile1">Dokumentasi Kegiatan</label>
-                    <input type="file" class="form-control-file" id="foto_kegiatan" name="foto_kegiatan">
+                    <input type="file" class="form-control-file" id="foto_kegiatan" name="foto_kegiatan" value="<?= $jalan['foto_kegiatan']; ?>">
                 </div>
                 <div class="form-group">
                     <label for="exampleFormControlFile1">Upload Video</label>
-                    <input type="file" class="form-control-file" id="video_kegiatan" name="video_kegiatan">
+                    <input type="file" class="form-control-file" id="video_kegiatan" name="video_kegiatan" value="<?= $jalan['video_kegiatan']; ?>">
                 </div>
                 <input class="btn btn-primary mt-3" type="submit" value="Submit">
             </div>
